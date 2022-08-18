d5c7022a87345168508b5a7fa1992247292db7ff
Murilo Ribeiro Cristiano <muricristiano@gmail.com>
2022-08-18 19:13:23 -0300

    Specialize Trail

---
+++ 3. ReactJS/projeto-react-do-modulo/src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+export function Home() {
+  return (
+    <>
+    <h1>Aqui manja React</h1>
+    <input type="text" placeholder="digite o nome..."/>
+    <button type="button">Adicionar</button>
+    </>
+  )
+}
